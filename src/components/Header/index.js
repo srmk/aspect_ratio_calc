1be1136bd56aa56dffcbd6ba23964cc758c29b4e
manikandan277 <manikandan.subramani@impigertech.com>
2021-06-10 10:18:20 +0530

    fixes updated

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,27 +2,27 @@ import React, { useEffect, useContext } from 'react';
 import { ThemeContext } from '../../theme/ThemeContext';
 
 function Header() {
-    const theme = useContext(ThemeContext);
+    // const theme = useContext(ThemeContext);
 
-    useEffect(() => {
-        let crescent = document.getElementById('crescent');
-        let toggle = document.getElementById('toggle');
-        if (theme.themeMode === 'dark-theme') {
-            crescent.style.transform = `scale(0.6)`;
-            toggle.style.transform = `translateX(85%)`;
-        }
-    }, [theme]);
+    // useEffect(() => {
+    //     let crescent = document.getElementById('crescent');
+    //     let toggle = document.getElementById('toggle');
+    //     if (theme.themeMode === 'dark-theme') {
+    //         crescent.style.transform = `scale(0.6)`;
+    //         toggle.style.transform = `translateX(85%)`;
+    //     }
+    // }, [theme]);
 
-    const toggleAction = (themeMode, changeThemeMode) => {
-        let crescent = document.getElementById('crescent');
-        let toggle = document.getElementById('toggle');
-        let isActive = (themeMode === 'light-theme') ? true : false;
-        let xPosition = isActive ? '85%' : '0';
-        let scale = isActive ? '0.6' : '0';
-        changeThemeMode(isActive);
-        crescent.style.transform = `scale(${scale})`;
-        toggle.style.transform = `translateX(${xPosition})`;
-    }
+    // const toggleAction = (themeMode, changeThemeMode) => {
+    //     let crescent = document.getElementById('crescent');
+    //     let toggle = document.getElementById('toggle');
+    //     let isActive = (themeMode === 'light-theme') ? true : false;
+    //     let xPosition = isActive ? '85%' : '0';
+    //     let scale = isActive ? '0.6' : '0';
+    //     changeThemeMode(isActive);
+    //     crescent.style.transform = `scale(${scale})`;
+    //     toggle.style.transform = `translateX(${xPosition})`;
+    // }
     return (
         <ThemeContext.Consumer>
             {({ themeMode, changeThemeMode }) => (
