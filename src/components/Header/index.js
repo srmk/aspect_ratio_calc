6c0b9a3fb9434fed0dc830d2717567290aafd771
Manikandan.S <39993258+srmk@users.noreply.github.com>
2021-06-22 12:52:51 +0530

    theme mode added

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,34 +1,34 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import { ThemeContext } from '../../theme/ThemeContext';
 
 function Header() {
-    // const theme = useContext(ThemeContext);
+    const theme = useContext(ThemeContext);
 
-    // useEffect(() => {
-    //     let crescent = document.getElementById('crescent');
-    //     let toggle = document.getElementById('toggle');
-    //     if (theme.themeMode === 'dark-theme') {
-    //         crescent.style.transform = `scale(0.6)`;
-    //         toggle.style.transform = `translateX(85%)`;
-    //     }
-    // }, [theme]);
+    useEffect(() => {
+        let crescent = document.getElementById('crescent');
+        let toggle = document.getElementById('toggle');
+        if (theme.themeMode === 'dark-theme') {
+            crescent.style.transform = `scale(0.6)`;
+            toggle.style.transform = `translateX(85%)`;
+        }
+    }, [theme]);
 
-    // const toggleAction = (themeMode, changeThemeMode) => {
-    //     let crescent = document.getElementById('crescent');
-    //     let toggle = document.getElementById('toggle');
-    //     let isActive = (themeMode === 'light-theme') ? true : false;
-    //     let xPosition = isActive ? '85%' : '0';
-    //     let scale = isActive ? '0.6' : '0';
-    //     changeThemeMode(isActive);
-    //     crescent.style.transform = `scale(${scale})`;
-    //     toggle.style.transform = `translateX(${xPosition})`;
-    // }
+    const toggleAction = (themeMode, changeThemeMode) => {
+        let crescent = document.getElementById('crescent');
+        let toggle = document.getElementById('toggle');
+        let isActive = (themeMode === 'light-theme') ? true : false;
+        let xPosition = isActive ? '85%' : '0';
+        let scale = isActive ? '0.6' : '0';
+        changeThemeMode(isActive);
+        crescent.style.transform = `scale(${scale})`;
+        toggle.style.transform = `translateX(${xPosition})`;
+    }
     return (
         <ThemeContext.Consumer>
             {({ themeMode, changeThemeMode }) => (
                 <header className={'app-header'}>
                     <h1><b>Aspect Ratio Calculator</b></h1>
-                    {/* <div className={'toggle-switch-container'}>
+                    <div className={'toggle-switch-container'}>
                         <input
                             type="checkbox"
                             id="switch"
@@ -42,7 +42,7 @@ function Header() {
                                 <p className="dark">Dark</p>
                             </div>
                         </label>
-                    </div> */}
+                    </div>
                 </header>
             )}
         </ThemeContext.Consumer>
